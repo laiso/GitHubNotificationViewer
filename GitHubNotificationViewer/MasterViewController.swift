8d19aa292c745144044ab161a9bd2359285bee55
laiso <laiso@lai.so>
2016-01-27 01:01:22 +0900

    Show notification titles and then open a content to click
    - Extracts GitHub.swift in ViewController
    - Add SwiftJSON in Carthage bundles
    - Open a URL with SFSafariViewController insted of DetailViewContorller-
    - Remove the unused code for UISplitView

--- GitHubNotificationViewer/MasterViewController.swift
+++ GitHubNotificationViewer/MasterViewController.swift
@@ -6,64 +6,20 @@
 //
 
 import UIKit
-import OAuthSwift
-
-class NotificationItem {
-    var title: String = "";
-}
+import SafariServices
 
 class MasterViewController: UITableViewController {
-    var detailViewController: DetailViewController? = nil
     var items: [NotificationItem] = [NotificationItem]()
-    var oauthswift: OAuth2Swift! = nil
+    var github = GitHub()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let split = self.splitViewController {
-            let controllers = split.viewControllers
-            self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
-        }
-        
         let refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: "onRefresh", forControlEvents: UIControlEvents.ValueChanged)
         self.refreshControl = refreshControl
     }
     
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        if (oauthswift == nil){
-            onSignInButton()
-        }
-    }
-    
-    override func viewWillAppear(animated: Bool) {
-        self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
-        super.viewWillAppear(animated)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Segues
-
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "showDetail" {
-            if let
-                indexPath = self.tableView.indexPathForSelectedRow,
-                navigation = segue.destinationViewController as? UINavigationController,
-                controller = navigation.topViewController as? DetailViewController
-            {
-                controller.detailItem = items[indexPath.row]
-                controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
-                controller.navigationItem.leftItemsSupplementBackButton = true
-            }
-        }
-    }
-
     // MARK: - Table View
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -82,50 +38,36 @@ class MasterViewController: UITableViewController {
         return cell
     }
     
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if let
+            indexPath = self.tableView.indexPathForSelectedRow,
+            url = items[indexPath.row].URL
+        {
+            let safari = SFSafariViewController(URL: url)
+            self.presentViewController(safari, animated: true, completion: nil)
+        }
+    }
+    
     // MARK: -
     
     func onRefresh() {
-        if let api = oauthswift {
-            api.client.get("https://api.github.com/notifications",
-                success: { (data, response) -> Void in
-                    print(data)
-                    
-                    dispatch_async(dispatch_get_main_queue(), {
-                        self.refreshControl?.endRefreshing()
-                    })
-                },
-                failure: { error in
-                    print(error.localizedDescription)
-                    
-                    dispatch_async(dispatch_get_main_queue(), {
-                        self.refreshControl?.endRefreshing()
-                    })
+        github.loadNotifications { (error, notifications) -> Void in
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                if let items = notifications {
+                    self.items = items
+                }
+                
+                self.refreshControl?.endRefreshing()
+                self.tableView.reloadData()
             })
         }
     }
     
     @IBAction
     func onSignInButton() {
-        oauthswift = OAuth2Swift(
-            consumerKey:    "139e6bcdad03cc9cf86f",
-            consumerSecret: "09145cc9459ed062604b4f51ad30f5286a123477",
-            authorizeUrl:   "https://github.com/login/oauth/authorize",
-            accessTokenUrl: "https://github.com/login/oauth/access_token",
-            responseType:   "code"
-        )
-        oauthswift.authorize_url_handler = SafariURLHandler(viewController: self)
-        
-        let state: String = generateStateWithLength(20) as String
-        oauthswift.authorizeWithCallbackURL(
-            NSURL(string: "oauth-swift://oauth-callback/github")!,
-            scope: "user,repo,notifications", state: state,
-            success: { credential, response, parameters in
-                self.onRefresh()
-            },
-            failure: { error in
-                print(error.localizedDescription)
-            }
-        )
+        github.authorizeWithViewController(self) { () -> Void in
+            self.onRefresh()
+        }
     }
     
 }
